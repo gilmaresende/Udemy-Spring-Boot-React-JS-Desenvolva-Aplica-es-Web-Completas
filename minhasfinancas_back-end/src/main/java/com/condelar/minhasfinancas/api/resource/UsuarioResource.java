079d06cb64b7a644c38dd4c80799303ff3ce26be
Gilmar F <gilmar74796@gmail.com>
2020-11-05 22:54:57 -0300

    Controle Salvar Usuario

--- minhasfinancas_back-end/src/main/java/com/condelar/minhasfinancas/api/resource/UsuarioResource.java
+++ minhasfinancas_back-end/src/main/java/com/condelar/minhasfinancas/api/resource/UsuarioResource.java
@@ -1,14 +1,37 @@
 package com.condelar.minhasfinancas.api.resource;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.condelar.minhasfinancas.api.dto.UsuarioDTO;
+import com.condelar.minhasfinancas.exception.RegraNegocioException;
+import com.condelar.minhasfinancas.model.entity.Usuario;
+import com.condelar.minhasfinancas.services.UsuarioService;
+
 @RestController
+@RequestMapping("/api/usuario")
 public class UsuarioResource {
-	
-	@GetMapping("/")
-	public String helloWorld() {
-		return"Hello World";
+
+	private UsuarioService service;
+
+	public UsuarioResource(UsuarioService service) {
+		this.service = service;
+	}
+
+	@PostMapping
+	public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
+		Usuario usuario = Usuario.builder().nome(dto.getNome()).email(dto.getEmail()).senha(dto.getSenha()).build();
+		try {
+			Usuario usuarioSalvo = service.salvarUsuario(usuario);
+			return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+		} catch (RegraNegocioException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+
 	}
 
 }
