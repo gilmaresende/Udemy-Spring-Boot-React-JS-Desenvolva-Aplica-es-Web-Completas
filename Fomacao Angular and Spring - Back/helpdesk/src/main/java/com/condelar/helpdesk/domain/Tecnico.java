cf3634d5a59030ed34b4f7cf8f50a80676ae328c
Gilmar <gilmaresende@gmail.com>
2023-02-13 23:58:55 -0300

    anotacoes jpa para classes de dominio

--- Fomacao Angular and Spring - Back/helpdesk/src/main/java/com/condelar/helpdesk/domain/Tecnico.java
+++ Fomacao Angular and Spring - Back/helpdesk/src/main/java/com/condelar/helpdesk/domain/Tecnico.java
@@ -3,16 +3,27 @@ package com.condelar.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.condelar.helpdesk.domain.enuns.Perfil;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Tecnico extends Pessoa {
 
-	private List<Chamado> chamados = new ArrayList();
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
+	private List<Chamado> chamados = new ArrayList<>();
 	
 	public Tecnico() {
 		super();
+		addPerfis(Perfil.TECNICO);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfis(Perfil.TECNICO);
 	}
 
 	public List<Chamado> getChamados() {
