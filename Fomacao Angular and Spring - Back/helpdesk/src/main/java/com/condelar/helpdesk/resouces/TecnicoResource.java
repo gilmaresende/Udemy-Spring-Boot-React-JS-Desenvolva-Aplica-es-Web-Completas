c248ddd50765b22e0ed2277e63a1dabda1e020fa
Gilmar <gilmaresende@gmail.com>
2023-02-20 19:20:58 -0300

    Tecnico find all

--- Fomacao Angular and Spring - Back/helpdesk/src/main/java/com/condelar/helpdesk/resouces/TecnicoResource.java
+++ Fomacao Angular and Spring - Back/helpdesk/src/main/java/com/condelar/helpdesk/resouces/TecnicoResource.java
@@ -1,5 +1,8 @@
 package com.condelar.helpdesk.resouces;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +25,13 @@ public class TecnicoResource {
 	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
 		Tecnico obj = service.findById(id);
 		return ResponseEntity.ok(new TecnicoDTO(obj));
+	}
+
+	@GetMapping
+	public ResponseEntity<List<TecnicoDTO>> findAll() {
+		List<Tecnico> list = service.findAll();
+		List<TecnicoDTO> listDTO = list.stream().map(m -> new TecnicoDTO(m)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
--- Fomacao Angular and Spring - Back/helpdesk/src/main/java/com/condelar/helpdesk/service/DBService.java
+++ Fomacao Angular and Spring - Back/helpdesk/src/main/java/com/condelar/helpdesk/service/DBService.java
@@ -17,36 +17,37 @@ import com.condelar.helpdesk.repositorys.TecnicoRepository;
 
 @Service
 public class DBService {
-	
 
 	@Autowired
 	private TecnicoRepository tecnicoRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private ChamadoRepository chamadoRepository;
-	
+
 	public void instanciaDB() {
-		
-		Tecnico t1 = new Tecnico(1, "Joao Souza", "123456"," joao.sousa@mail.com", "s1235879");
+
+		Tecnico t1 = new Tecnico(1, "Gilmar Fabiano", "123", " gfresende@mail.com", "s1235879");
 		t1.addPerfis(Perfil.ADMIN);
-		Tecnico t2 = new Tecnico(1, "Maria Souza", "7412589"," maria.sousa@mail.com", "df7591");
-		
-		
-		
-		
+		Tecnico t2 = new Tecnico(2, "Geraldo Henrique", "124", " ghmorais@mail.com", "df7591");
+		t2.addPerfis(Perfil.ADMIN);
+		Tecnico t3 = new Tecnico(3, "Italo Medice", "125", " itmedice.com", "gdfuh47");
+		t3.addPerfis(Perfil.ADMIN);
+		Tecnico t4 = new Tecnico(4, "Jadson Moura", "126", " jamoura.com", "drew125");
+		t4.addPerfis(Perfil.ADMIN);
+
 		Cliente c1 = new Cliente(1, "Tadeu Borges", "785196", "tadeu.borges@mail.com", "4532gf97");
 		Cliente c2 = new Cliente(2, "Maria Julia", "785196", "maria.julia@mail.com", "fds72456");
-		
-		
-		Chamado ch1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Primeiro Chamado", "Primeira Instacia", t2, c2);
-		
-		tecnicoRepository.saveAll(Arrays.asList(t1));
+
+		Chamado ch1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Primeiro Chamado", "Primeira Instacia", t2,
+				c2);
+
+		tecnicoRepository.saveAll(Arrays.asList(t1, t2, t3, t4));
 		clienteRepository.saveAll(Arrays.asList(c1));
 		chamadoRepository.saveAll(Arrays.asList(ch1));
-		
+
 	}
 
 }
