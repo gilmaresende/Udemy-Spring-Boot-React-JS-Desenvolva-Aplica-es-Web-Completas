377b10d118ef707b4abbc74d88fa33ff90fea5b9
Gilmar <gilmaresende@gmail.com>
2023-02-22 19:34:23 -0300

    find all - Chamados

--- Fomacao Angular and Spring - Back/helpdesk/src/main/java/com/condelar/helpdesk/resouces/ChamadoResouce.java
+++ Fomacao Angular and Spring - Back/helpdesk/src/main/java/com/condelar/helpdesk/resouces/ChamadoResouce.java
@@ -1,5 +1,8 @@
 package com.condelar.helpdesk.resouces;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,14 +17,20 @@ import com.condelar.helpdesk.service.ChamadoService;
 @RestController
 @RequestMapping(value = "/chamados")
 public class ChamadoResouce {
-	
+
 	@Autowired
 	private ChamadoService service;
-	
-	@GetMapping(value="/{id}")
-	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
 		Chamado ob = service.findById(id);
 		return ResponseEntity.ok().body(new ChamadoDTO(ob));
+	}
+
+	@GetMapping
+	public ResponseEntity<List<ChamadoDTO>> findAll() {
+		List<Chamado> all = service.findAll();
+		return ResponseEntity.ok().body(all.stream().map(m -> new ChamadoDTO(m)).collect(Collectors.toList()));
 	}
 
 }
--- Fomacao Angular and Spring - Back/helpdesk/src/main/java/com/condelar/helpdesk/service/ChamadoService.java
+++ Fomacao Angular and Spring - Back/helpdesk/src/main/java/com/condelar/helpdesk/service/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.condelar.helpdesk.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,4 +21,7 @@ public class ChamadoService {
 		return ob.orElseThrow(() -> new ObjectNotFoundException("Chamado Não encontrado! Id:" + id));
 	}
 
+	public List<Chamado> findAll() {
+		return repository.findAll();
+	}
 }
