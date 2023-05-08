2c21d8afb0e916a435dae08ab90f9aa0190b0f50
Gilmar <gilmaresende@gmail.com>
2023-05-08 19:57:17 -0300

    Domain Categoria

--- Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB/cursomc/src/main/java/com/condelar/cursomc/resources/CategoriaResource.java
+++ Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB/cursomc/src/main/java/com/condelar/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package com.condelar.cursomc.resources;
 
+import com.condelar.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "Rest está funcionando!";
+    public List<Categoria> listar() {
+
+        Categoria c1 = new Categoria(1, "Informatica");
+        Categoria c2 = new Categoria(2, "Escritorio");
+
+        List<Categoria> list = new ArrayList<>();
+        list.add(c1);
+        list.add(c2);
+        return list;
     }
 }
