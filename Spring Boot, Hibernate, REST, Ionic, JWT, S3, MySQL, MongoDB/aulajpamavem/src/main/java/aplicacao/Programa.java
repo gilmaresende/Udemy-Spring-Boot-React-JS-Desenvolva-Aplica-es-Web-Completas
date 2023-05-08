86eee90fff9b1aebdf8d6fdfd01142880c93f8ac
Gilmar <gilmaresende@gmail.com>
2023-05-08 18:56:24 -0300

    nivelamento jpa

--- Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB/aulajpamavem/src/main/java/aplicacao/Programa.java
+++ Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB/aulajpamavem/src/main/java/aplicacao/Programa.java
@@ -8,24 +8,42 @@ import dominio.Pessoa;
 
 public class Programa {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("java16Jpa");
+        EntityManager em = emf.createEntityManager();
 
-		Pessoa p1 = new Pessoa(null, "Joao", "Joao@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Maria", "Maria@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Pedro", "Pedro@gmail.com");
+        remove(em);
+        em.close();
+        emf.close();
+        System.out.println("Pronto!");
+    }
 
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("java16Jpa");
-		EntityManager em = emf.createEntityManager();
+    private static void remove(EntityManager em) {
+        em.getTransaction().begin();
+        Pessoa p = em.find(Pessoa.class, 5);
+        em.remove(p);
+        em.getTransaction().commit();
+    }
 
-		em.getTransaction().begin();
+    private static void reade(EntityManager em) {
+        Pessoa p = em.find(Pessoa.class, 5);
+        System.out.println(p);
+    }
 
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
+    private static void save(EntityManager em) {
+        Pessoa p1 = new Pessoa(null, "Joao 2", "Joao@gmail.com");
+        Pessoa p2 = new Pessoa(null, "Maria 2", "Maria@gmail.com");
+        Pessoa p3 = new Pessoa(null, "Pedro 2", "Pedro@gmail.com");
 
-		em.getTransaction().commit();
-		System.out.println("Pronto!");
+        em.getTransaction().begin();
 
-	}
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+
+        em.getTransaction().commit();
+        System.out.println("Pronto!");
+    }
+
 
 }
