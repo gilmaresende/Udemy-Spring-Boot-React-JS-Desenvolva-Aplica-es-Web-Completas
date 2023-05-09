4d8351aa27476afc26c85a2256a6366f019fd2e2
Gilmar <gilmaresende@gmail.com>
2023-05-09 08:18:30 -0300

    operation of instanciation

--- Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB/cursomc/src/main/java/com/condelar/cursomc/CursomcApplication.java
+++ Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB/cursomc/src/main/java/com/condelar/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.condelar.cursomc;
 
+import com.condelar.cursomc.domain.Categoria;
+import com.condelar.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CursomcApplication.class, args);
-	}
+    @Autowired
+    CategoriaRepository categoriaRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(CursomcApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+        Categoria cat3 = new Categoria(null, "Gamer");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
+    }
 }
