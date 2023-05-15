713077956fb1e2ca35cd7dd5173c9d14d5e97685
Gilmar <gilmaresende@gmail.com>
2023-05-15 20:35:54 -0300

    Revert "ajuste repository"
    This reverts commit b977e24bec78e1c9501f50a35c11d5a4aec8592d.

---
+++ treinamento_spring-by_nelio_alvez-2023/dllist/src/test/java/com/devsuperior/dllist/DllistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dllist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DllistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
