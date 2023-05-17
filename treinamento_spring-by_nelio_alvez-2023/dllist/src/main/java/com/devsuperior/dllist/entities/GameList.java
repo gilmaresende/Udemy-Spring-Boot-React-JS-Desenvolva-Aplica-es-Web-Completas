fa5645eac9c668e24e4600a1918711bf2b4e6757
Gilmar <gilmaresende@gmail.com>
2023-05-17 13:00:14 -0300

    domain model

---
+++ treinamento_spring-by_nelio_alvez-2023/dllist/src/main/java/com/devsuperior/dllist/entities/GameList.java
@@ -0,0 +1,63 @@
+package com.devsuperior.dllist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_game_list")
+public class GameList {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String title;
+
+	public GameList() {
+	}
+
+	public GameList(Long id, String title) {
+		super();
+		this.id = id;
+		this.title = title;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GameList other = (GameList) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
