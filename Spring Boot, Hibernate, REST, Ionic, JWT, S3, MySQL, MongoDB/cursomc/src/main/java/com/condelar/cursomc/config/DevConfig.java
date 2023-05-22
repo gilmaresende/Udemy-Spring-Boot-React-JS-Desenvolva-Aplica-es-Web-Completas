52a519499171001109396cf7a774cb62bff3dd01
Gilmar <gilmaresende@gmail.com>
2023-05-22 13:07:51 -0300

    criando profile dev

---
+++ Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB/cursomc/src/main/java/com/condelar/cursomc/config/DevConfig.java
@@ -0,0 +1,30 @@
+package com.condelar.cursomc.config;
+
+import com.condelar.cursomc.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String stategy;
+
+    @Bean
+    public boolean instanciateDatabse() throws ParseException {
+        if (!"create".equals(stategy))
+            return false;
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+
+}
