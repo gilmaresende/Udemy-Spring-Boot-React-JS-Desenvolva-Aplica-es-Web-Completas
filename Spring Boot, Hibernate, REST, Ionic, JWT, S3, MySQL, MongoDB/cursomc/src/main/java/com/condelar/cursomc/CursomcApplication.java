e1125974ea3b23f13c12fe7d10b803004eedfe8c
Gilmar FL Resende <gilmaresende@gmail.com>
2023-07-10 21:46:57 -0300

    Salvando primeiro arquivo no S3

--- Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB/cursomc/src/main/java/com/condelar/cursomc/CursomcApplication.java
+++ Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB/cursomc/src/main/java/com/condelar/cursomc/CursomcApplication.java
@@ -1,5 +1,7 @@
 package com.condelar.cursomc;
 
+import com.condelar.cursomc.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+	@Autowired
+	S3Service s3Service;
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, "--debug");
 		//SpringApplication.run(CursomcApplication.class, args);
@@ -14,7 +18,7 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("C:\\Users\\Gilmar\\Desktop\\tmp\\Corretto-Duke-no-text-1024x536.jpg");
 
 	}
 
