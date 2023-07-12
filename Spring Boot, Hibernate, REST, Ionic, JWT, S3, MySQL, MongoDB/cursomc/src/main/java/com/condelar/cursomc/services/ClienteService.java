3161a3fa2d453a3aecbe582d3de45a2b9bec1f21
Gilmar FL Resende <gilmaresende@gmail.com>
2023-07-11 23:39:53 -0300

    Usando padrão de nomes para imagens

--- Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB/cursomc/src/main/java/com/condelar/cursomc/services/ClienteService.java
+++ Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB/cursomc/src/main/java/com/condelar/cursomc/services/ClienteService.java
@@ -14,6 +14,7 @@ import com.condelar.cursomc.services.exception.AuthorizationException;
 import com.condelar.cursomc.services.exception.DataIntegrityException;
 import com.condelar.cursomc.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -23,6 +24,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -40,7 +42,13 @@ public class ClienteService {
     private BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
+    private ImageService imageService;
+
+    @Autowired
     private S3Service s3Service;
+
+    @Value("${img.prefix.client.profile}")
+    private String prefix;
 
     public Cliente find(Integer id) {
 
@@ -127,10 +135,13 @@ public class ClienteService {
             throw new AuthorizationException("Acesso Negado");
         }
 
+        BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+        String fileName = prefix + user.getId() + ".jpg";
+
         Optional<Cliente> op = repo.findById(user.getId());
 
         Cliente cliente = op.get();
-        URI uri = s3Service.uploadFile(multipartFile);
+        URI uri = s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
         cliente.setImageUrl(uri.toString());
         repo.save(cliente);
         return uri;
