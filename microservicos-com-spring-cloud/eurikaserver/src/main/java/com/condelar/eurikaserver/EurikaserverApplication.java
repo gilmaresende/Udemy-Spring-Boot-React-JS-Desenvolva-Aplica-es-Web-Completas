a49926340f144e319b539aaa7ebb1105a196b797
Gilmar FL Resende <gilmaresende@gmail.com>
2023-07-26 18:41:31 -0300

    start-project

---
+++ microservicos-com-spring-cloud/eurikaserver/src/main/java/com/condelar/eurikaserver/EurikaserverApplication.java
@@ -0,0 +1,13 @@
+package com.condelar.eurikaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurikaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurikaserverApplication.class, args);
+	}
+
+}
